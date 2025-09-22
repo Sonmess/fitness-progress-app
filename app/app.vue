e2da65777155da086e5896ebd9292b0496fc0a41
bohac <matej.bohac@ringier.sk>
2025-09-22 15:35:57 +0200

    ~ prve zmeny

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <main>
+    <header>
+      <h1>Fitness progress</h1>
+      <nav>Navigacia</nav>
+    </header>
+
+    <section>Telo</section>
+
+    <footer>Paticka</footer>
+  </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  devServer: {
+    host: "127.0.0.1",
+    port: 3000,
+  },
+});
